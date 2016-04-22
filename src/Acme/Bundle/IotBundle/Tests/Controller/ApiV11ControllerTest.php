dff9eb44b0733dbbe4535ef7b124108ad2b950b6
ubuntu12_lab <ubuntu12_924@ubuntu.com>
2016-04-23 03:00:26 +0800

    remove data

--- src/Acme/Bundle/IotBundle/Tests/Controller/ApiV11ControllerTest.php
+++ src/Acme/Bundle/IotBundle/Tests/Controller/ApiV11ControllerTest.php
@@ -10,11 +10,20 @@ class ApiV11ControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
+        $queryStr = '70c9fb4c4bc5c93b9647d2ad9c80d9406acd9671' . 
+                    'edafbb1f35267678588bcda8e714d1b5d0629b42' . 
+                    '5934e243538feb102441c7a594e0159fb3f1d585' . 
+                    'afa5dc920accce9167607607114c46111b5069fe' . 
+                    '3377a0e2dea639acd176af2e18e17e24203888e6' . 
+                    'f744964f412283a9';
+
         $crawler = $client->request('GET', 
-                                    '/iotdev/v1.1/send?sn=12345asdf&' .
-                                    'value=1-10-1461041182123_2-120-1461041182123&' .
-                                    'vender=jimupai&model=JMA01&random=1c2a448f15ce5149'
+                                    'iotdev/v1.1/send?' . $queryStr
                                     );
+
+        // $crawler = $client->request('GET', '/');
+
+        printf("\nTest Get URL success: \n" . $client->getRequest()->getUri() . "\n");
 
         $this->assertEquals(
                             200, // or Symfony\Component\HttpFoundation\Response::HTTP_OK
@@ -25,6 +34,91 @@ class ApiV11ControllerTest extends WebTestCase
     public function testParseData()
     {
 
+    }
+
+    /**
+     * @dataProvider getDecrptStrings
+     */
+    public function testApiV11Send($string, $key, $expected_string)
+    {
+        static::bootKernel();
+        $security = static::$kernel->getContainer()->get('acme.iot.security');
+        $decryptStr = $security->decryptAlert($string);
+
+        printf("\nTest Security encrypt str: \n" . "string:" . $string . "\nexpected string:" . $expected_string . "\n");
+        $this->assertEquals($expected_string, $decryptStr);
+
+        $client = static::createClient();
+        $crawler = $client->request('GET', 
+                                    'iotdev/v1.1/send?' . $string
+                                    );
+
+        $result = $this->removeSpace($client->getResponse()->getContent());
+        $correct_result = preg_match('/^result:[A-Za-z0-9]{10,}/i', $result);
+        
+
+        printf("\nTest Correct response format:\n" . $result . "\n");
+        $this->assertEquals(true, $correct_result);
+
+        $result_code = explode(':', $result)[1];
+        $decryptResult = $security->decryptAlert($result_code);
+        printf("Test Response code:\n" . $decryptResult . "\n");
+    }
+
+    /**
+     * @dataProvider getIncorrectDecrptStrings
+     */
+    public function testApiV11IncorrectSend($string, $key)
+    {
+        static::bootKernel();
+        $security = static::$kernel->getContainer()->get('acme.iot.security');
+        $decryptStr = $security->decryptAlert($string);
+
+        $client = static::createClient();
+        $crawler = $client->request('GET', 
+                                    'iotdev/v1.1/send?' . $string
+                                    );
+
+        $result = $this->removeSpace($client->getResponse()->getContent());
+        $correct_result = preg_match('/^result:[A-Za-z0-9]{10,}/i', $result);        
+
+        printf("\nTest Correct response format:\n" . $result . "\n");
+        $this->assertEquals(true, $correct_result);
+
+        $result_code = explode(':', $result)[1];
+        $decryptResult = $security->decryptAlert($result_code);
+        printf("Test Response code:\n" . $decryptResult . "\n");
+    }
+
+    public function getEncrptStrings()
+    {
+        return array(
+                    array('sn=asdf12345&value=1-10-1461041182123_2-20-1461041182123&vender=jimupai&model=JMA01&random=1c2a448f15ce5149',
+                          'TTuuIvb76TY123Ki',
+                          '70c9fb4c4bc5c93b9647d2ad9c80d9406acd9671edafbb1f35267678588bcda8e714d1b5d0629b425934e243538feb102441c7a594e0159fb3f1d585afa5dc920accce9167607607114c46111b5069fe3377a0e2dea639acd176af2e18e17e24203888e6f744964f412283a9')
+        );
+    }
+
+    public function getDecrptStrings()
+    {
+        return array(
+                    array('70c9fb4c4bc5c93b9647d2ad9c80d9406acd9671edafbb1f35267678588bcda8e714d1b5d0629b425934e243538feb102441c7a594e0159fb3f1d585afa5dc920accce9167607607114c46111b5069fe3377a0e2dea639acd176af2e18e17e24203888e6f744964f412283a9',
+                          'TTuuIvb76TY123Ki',
+                          'sn=asdf12345&value=1-10-1461041182123_2-20-1461041182123&vender=jimupai&model=JMA01&random=1c2a448f15ce5149')
+        );
+    }
+
+    public function getIncorrectDecrptStrings()
+    {
+        return array(
+                    array('4bc5c93b9647d2ad9c80d9406acd9671e5267678588bcda8e714d1b5d0629b425934e243538feb102441c7a594e0159fb3f1d585afa5dc920accce9167607607114c46111b5069fe3377a0e2dea639acd176af2e18e17e24203888e6f744964f412283a9',
+                          'Wrongkey')
+        );
+    }
+
+    private function removeSpace($str)
+    {
+        return strtolower(preg_replace("/\s+|　/", "", urldecode($str)));
     }
 
     /*
