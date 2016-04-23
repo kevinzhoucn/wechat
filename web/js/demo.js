8b4346c5215cc49879066659e6d8353933c26e55
zzpf <zzpf@163.com>
2016-04-23 17:46:15 +0800

    wifi 02

--- web/js/demo.js
+++ web/js/demo.js
@@ -3,7 +3,6 @@
     wx.invoke('configWXDeviceWiFi', {}, function(res){
       if(res.err_msg == 'configWXDeviceWiFi:ok'){
         alert('配置成功!');
-        wx.closeWindow();
       } else {
         alert('配置失败！请重试');
       }
