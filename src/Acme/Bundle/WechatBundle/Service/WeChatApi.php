ff9facb8e379ecb2be20c72cadc957d8864b9289
ubuntu12_lab <ubuntu12_924@ubuntu.com>
2016-04-23 11:03:07 +0800

    get log for webchat request

--- src/Acme/Bundle/WechatBundle/Service/WeChatApi.php
+++ src/Acme/Bundle/WechatBundle/Service/WeChatApi.php
@@ -43,6 +43,12 @@ class WeChatApi
             $result = "error: signature not matched!";
         }
 
+        $info = sprintf("get request url info: %s", $request->getUri());
+        $this->toLog($info);
+
+        $info = sprintf("get request query info: %s", $request->getQueryString());
+        $this->toLog($info);
+
         $info = sprintf("signature: \"%s\", timestamp: \"%s\", nonce: \"%s\", echostr: \"%s\"", $signature, $timestamp, $nonce, $echoStr);
         $this->toLog($info);
 
